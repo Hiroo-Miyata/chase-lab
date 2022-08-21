d843a665a9adde7b3d55fa443ca2ea8048c34804
宮田寛生 <hiroo@FunamaruMac.local>
2022-08-21 18:06:57 -0400

    visualize 10d metadata

--- src/normalizeByDay.m
+++ src/normalizeByDay.m
@@ -7,46 +7,63 @@ endofTrialByDays = zeros(length(files)+1, 1);
 titles = {'Day1', 'Day2', 'Day3', 'Day4', 'Day5', 'Day6', 'Day7', 'Day8', 'Day9', 'Day10', 'Day11', 'Day12'};
 means = zeros(length(files), emg_channel);
 stds = zeros(length(files), emg_channel);
-% for t=(1:length(files)) %(1:length(files)
-%     file = load('../data/processed/singleTrials_Rocky2022'+files(t)+'_movave_50ms.mat');
-%     singleTrialData = file.singleTrialData;
-%     
-%     dataLength = 0;
-%     for i=(1:length(singleTrialData))
-%         stateTransition = singleTrialData(i).prop.stateTransition;
-%         if all(ismember([3 4 5 6], stateTransition(1,:))) == 1
-%             dataLength + dataLength + 1;
-%         end
-%     end
-%     
-%     s = 0;
-%     EMG = zeros(801, emg_channel, dataLength);
-%     directionArray = zeros(dataLength);
-%     rewardArray = zeros(dataLength);
-%     for i=(1:length(singleTrialData))
-%         stateTransition = singleTrialData(i).prop.stateTransition;
-%         if all(ismember([3 4 5 6], stateTransition(1,:))) == 1
-%             s = s+1;
-%             GoCueTime = stateTransition(2, find(stateTransition(1, :)==4));
-%             % start: -200ms end: +600ms at GoCue
-%             EMGaroundGoCue = singleTrialData(i).emg(GoCueTime-200:GoCueTime+600, :);
-%             EMG(:,:, s) = EMGaroundGoCue;
-%             directionArray(s) = singleTrialData(i).prop.direction;
-%             rewardArray(s) = singleTrialData(i).prop.reward;
-%         end
-%     end
-%     
-%     meanEMGEachTrial = mean(EMG, 1);
-%     meanEMGEachTrial = reshape(meanEMGEachTrial, 5, []);
-%     endofTrialByDays(t+1) = endofTrialByDays(t) + int16(size(meanEMGEachTrial, 2));
-%     EMGAcrossDays = [EMGAcrossDays meanEMGEachTrial];
-%     emgRest = file.emg_rest; % 120s * channel
-%     baseline = zeros(size(meanEMGEachTrial)) + reshape(mean(emgRest, 1), 5, []);
-%     baselines = [baselines baseline];
-%     means(t, :) = mean(meanEMGEachTrial, 2);
-%     stds(t, :) = std(meanEMGEachTrial, 0, 2);
-% end
-% 
+tenDmetadataAcrossDays = zeros(10, emg_channel, length(files));
+
+for t=(1:length(files)) %(1:length(files)
+    file = load('../data/processed/singleTrials_Rocky2022'+files(t)+'_movave_50ms.mat');
+    singleTrialData = file.singleTrialData;
+    
+    dataLength = 0;
+    for i=(1:length(singleTrialData))
+        stateTransition = singleTrialData(i).prop.stateTransition;
+        if all(ismember([3 4 5 6 7], stateTransition(1,:))) == 1
+            dataLength + dataLength + 1;
+        end
+    end
+    
+    s = 0;
+    EMG = zeros(801, emg_channel, dataLength);
+    directionArray = zeros(dataLength);
+    rewardArray = zeros(dataLength);
+    for i=(1:length(singleTrialData))
+        stateTransition = singleTrialData(i).prop.stateTransition;
+        if all(ismember([3 4 5 6 7], stateTransition(1,:))) == 1
+            s = s+1;
+            GoCueTime = stateTransition(2, find(stateTransition(1, :)==4));
+            % start: -200ms end: +600ms at GoCue
+            EMGaroundGoCue = singleTrialData(i).emg(GoCueTime-200:GoCueTime+600, :);
+            EMG(:,:, s) = EMGaroundGoCue;
+            directionArray(s) = singleTrialData(i).prop.direction;
+            rewardArray(s) = singleTrialData(i).prop.reward;
+        end
+    end
+    
+    meanEMGEachTrial = mean(EMG, 1);
+    meanEMGEachTrial = reshape(meanEMGEachTrial, 5, []);
+    endofTrialByDays(t+1) = endofTrialByDays(t) + int16(size(meanEMGEachTrial, 2));
+    EMGAcrossDays = [EMGAcrossDays meanEMGEachTrial];
+    emgRest = file.emg_rest; % 120s * channel
+    baseline = zeros(size(meanEMGEachTrial)) + reshape(mean(emgRest, 1), 5, []);
+    baselines = [baselines baseline];
+    means(t, :) = mean(meanEMGEachTrial, 2);
+    stds(t, :) = std(meanEMGEachTrial, 0, 2);
+    tenDmetadataAcrossDays(9, :, t) = means(t,:);
+    tenDmetadataAcrossDays(10, :, t) = stds(t,:);
+    for direction=(1:8)
+        oneDirectionEMG = EMG(:,:,directionArray==direction);
+        meanOneDirectionEMG = mean(oneDirectionEMG, 3);
+        MaxIntensitysAtOneDirection = max(meanOneDirectionEMG, [], 1);
+        tenDmetadataAcrossDays(direction, :, t) = MaxIntensitysAtOneDirection;
+    end
+end
+
+for channel=(5:5)
+    figure
+    Y = reshape(tenDmetadataAcrossDays(:, channel, :), 10, []);
+    plot(Y)
+    title(file.muscleLabel(channel));
+    legend(titles)
+end
 % x=1:12;
 % for c=(1:1)
 %     figure
@@ -55,6 +72,8 @@ stds = zeros(length(files), emg_channel);
 %     xlabel('Mean')
 %     ylabel('Standard')
 % end
+
+
 
 % for c=(1:5) % !!!!!!!!!
 %     figure
@@ -71,46 +90,46 @@ stds = zeros(length(files), emg_channel);
 % end
 
 
-for t=(7:7) %length(files)
-    file = load('../data/processed/singleTrials_Rocky2022'+files(t)+'_movave_50ms.mat');
-    singleTrialData = file.singleTrialData;
-    emg_channel = 5;
-    
-    dataLength = 0;
-    for i=(1:length(singleTrialData))
-        stateTransition = singleTrialData(i).prop.stateTransition;
-        if all(ismember([3 4 5 6], stateTransition(1,:))) == 1
-            dataLength + dataLength + 1;
-        end
-    end
-    
-    s = 0;
-    EMG = zeros(801, emg_channel, dataLength);
-    directionArray = zeros(dataLength);
-    rewardArray = zeros(dataLength);
-    for i=(1:length(singleTrialData))
-        stateTransition = singleTrialData(i).prop.stateTransition;
-        if all(ismember([3 4 5 6], stateTransition(1,:))) == 1
-            s = s+1;
-            GoCueTime = stateTransition(2, find(stateTransition(1, :)==4));
-            % start: -200ms end: +600ms at GoCue
-            EMGaroundGoCue = singleTrialData(i).emg(GoCueTime-200:GoCueTime+600, :);
-            EMG(:,:, s) = EMGaroundGoCue;
-            directionArray(s) = singleTrialData(i).prop.direction;
-            rewardArray(s) = singleTrialData(i).prop.reward;
-        end
-    end
-    
-    meanEMGEachTrial = mean(EMG, 1);
-    meanEMGEachTrial = reshape(meanEMGEachTrial, 5, []);
-    endofTrialByDays(t+1) = endofTrialByDays(t) + int16(size(meanEMGEachTrial, 2));
-    emgRest = file.emg_rest; % 120s * channel
-    baselines = reshape(mean(emgRest, 1), 5, []);
-    normalizedEMG = (meanEMGEachTrial - baselines) ./ (mean(meanEMGEachTrial, 2) - baselines);
-    EMGAcrossDays = [EMGAcrossDays normalizedEMG];
-    means(t, :) = mean(normalizedEMG, 2);
-    stds(t, :) = std(normalizedEMG, 0, 2);
-end
+% for t=(7:7) %length(files)
+%     file = load('../data/processed/singleTrials_Rocky2022'+files(t)+'_movave_50ms.mat');
+%     singleTrialData = file.singleTrialData;
+%     emg_channel = 5;
+%     
+%     dataLength = 0;
+%     for i=(1:length(singleTrialData))
+%         stateTransition = singleTrialData(i).prop.stateTransition;
+%         if all(ismember([3 4 5 6 7], stateTransition(1,:))) == 1
+%             dataLength + dataLength + 1;
+%         end
+%     end
+%     
+%     s = 0;
+%     EMG = zeros(801, emg_channel, dataLength);
+%     directionArray = zeros(dataLength);
+%     rewardArray = zeros(dataLength);
+%     for i=(1:length(singleTrialData))
+%         stateTransition = singleTrialData(i).prop.stateTransition;
+%         if all(ismember([3 4 5 6 7], stateTransition(1,:))) == 1
+%             s = s+1;
+%             GoCueTime = stateTransition(2, find(stateTransition(1, :)==4));
+%             % start: -200ms end: +600ms at GoCue
+%             EMGaroundGoCue = singleTrialData(i).emg(GoCueTime-200:GoCueTime+600, :);
+%             EMG(:,:, s) = EMGaroundGoCue;
+%             directionArray(s) = singleTrialData(i).prop.direction;
+%             rewardArray(s) = singleTrialData(i).prop.reward;
+%         end
+%     end
+%     
+%     meanEMGEachTrial = mean(EMG, 1);
+%     meanEMGEachTrial = reshape(meanEMGEachTrial, 5, []);
+%     endofTrialByDays(t+1) = endofTrialByDays(t) + int16(size(meanEMGEachTrial, 2));
+%     emgRest = file.emg_rest; % 120s * channel
+%     baselines = reshape(mean(emgRest, 1), 5, []);
+%     normalizedEMG = (meanEMGEachTrial - baselines) ./ (mean(meanEMGEachTrial, 2) - baselines);
+%     EMGAcrossDays = [EMGAcrossDays normalizedEMG];
+%     means(t, :) = mean(normalizedEMG, 2);
+%     stds(t, :) = std(normalizedEMG, 0, 2);
+% end
 % 
 % for c=(1:1) % !!!!!!!!!
 %     figure
